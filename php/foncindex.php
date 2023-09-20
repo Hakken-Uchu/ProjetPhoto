ea1ebdcc9f00de64bdea3c70bdfb9cd45678401c
Jérémy Jalladeau <hitgame444@gmail.com>
2023-09-20 05:53:43 +0200

    newsletter

--- php/foncindex.php
+++ php/foncindex.php
@@ -1,17 +1,29 @@
 <?php
-$mysqlConnection = new PDO('mysql:host=localhost;dbname=capyfoot;charset=utf8', 'root', 'lol');
-$db = new PDO(
-    'mysql:host=localhost;dbname=capyfoot;charset=utf8',
-    'root',
-    'lol',
-    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],
-);
-try {
+$mysqlConnection = new PDO('mysql:host=localhost;dbname=mailing;charset=utf8', 'root', 'lol');
 
-} catch (PDOException $e) {
-    die($e->getMessage());
+
+$sqlQuery = 'SELECT * FROM `mailing`';
+$mailStatement = $mysqlConnection->prepare($sqlQuery);
+$mailStatement->execute();
+$mail = $mailStatement->fetchAll();
+
+
+
+if (isset($_POST['mail'])) {
+    $sqlQuery = 'INSERT INTO mailing(mail) VALUES (:mail)';
+    $insertRecipe = $mysqlConnection->prepare($sqlQuery);
+    $insertRecipe->execute([
+        'mail' => $_POST['mail'],
+    ]);
+    $message_mailing_success = "Votre inscription a bien été effectuer, merci!";
+    echo "<script type='text/javascript'>alert('$message_mailing_success');
+    document.location.href = '/';
+</script>";
+}else {
+    $message_mailing_fail = "Un problème est survenue veuiller réssayer plus tard";
+    echo "<script type='text/javascript'>alert('$message_mailing_fail');
+</script>";
 }
-
 
 
 ?>
